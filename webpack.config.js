17acef7a3544ff7a11a6de78249905f8fced2f6d
erikmunson <erik@ipsumcreative.com>
2017-08-12 22:54:33 -0700

    Initial repo setup
    Setup webpack, babel, some initial file structure, and a .gitignore

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {
+    contentBase: './dist'
+  },
+  devtool: process.env.NODE_ENV === 'production' ? false : 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['es2015'],
+            plugins: ['transform-object-rest-spread']
+          }
+        }
+      }
+    ]
+  }
+};
